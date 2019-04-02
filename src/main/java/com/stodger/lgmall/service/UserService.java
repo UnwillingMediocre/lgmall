a869ef7aefeed699e45501ec4ed0afa4fbdf500e
e <1014165248@qq.com>
2019-04-02 22:44:08 +0800

    project init commit

---
+++ src/main/java/com/stodger/lgmall/service/UserService.java
@@ -0,0 +1,11 @@
+package com.stodger.lgmall.service;
+
+/**
+ * @program: lgmall
+ * @description:
+ * @author: Stodger
+ * @create: 2019-04-02 22:35
+ * @Version: 1.0
+ **/
+public interface UserService {
+}
